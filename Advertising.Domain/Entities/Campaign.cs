11012d7b969a6868a04faa21d80cf307880967d4
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-03 15:41:15 +0100

    Added an Enum and an Campaign entity

--- Advertising.Domain/Entities/Campaign.cs
+++ Advertising.Domain/Entities/Campaign.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Advertising.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
---
+++ Advertising.Domain/Enums/CampaignStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Domain.Enums
+{
+    public enum CampaignStatus
+    {
+        Draft = 0,
+        Pending = 1,
+        Paid = 2,
+        Running = 3,
+        Completed = 4
+    }
+}
