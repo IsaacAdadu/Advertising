1beed24819227dc346fb5351c21f2b728d7f137a
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-03 15:40:34 +0100

    commit

---
+++ Advertising.Domain/DTOs/CreateCampaignDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Domain.DTOs
+{
+    public class CreateCampaignDto
+    {
+        public string Name { get; set; } = default!;
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public Guid OwnerId { get; set; }
+        public decimal Amount { get; set; }
+        public System.Collections.Generic.List<int> LocationIds { get; set; } = new();
+        public System.Collections.Generic.List<IFormFile>? Banners { get; set; }
+    }
+}
