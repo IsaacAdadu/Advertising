566eed10f47dc9d1df6f6bbd69585906d8a155a7
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-05 19:06:10 +0100

    Added RegisterUser Command

---
+++ Advertising.Application/Auth/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Advertising.Domain.Entities;
+using Advertising.Infrastructure.Security;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Auth.Commands.RegisterUser
+{
+    public class RegisterUserCommandHandler: IRequestHandler<RegisterUserCommand, AuthResult>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IJwtTokenService _jwtService;
+
+        public RegisterUserCommandHandler(UserManager<User> userManager, IJwtTokenService jwtService)
+        {
+            _userManager = userManager;
+            _jwtService = jwtService;
+        }
+
+        public async Task<AuthResult> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User
+            {
+                UserName = request.Email,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                ProfileCompleted = false
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded)
+            {
+                return new AuthResult
+                {
+                    Success = false,
+                    Message = string.Join("; ", result.Errors.Select(e => e.Description))
+                };
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+            var token = _jwtService.GenerateToken(user, roles);
+
+            return new AuthResult { Success = true, Token = token, Message = "User registered successfully" };
+        }
+    }
+}
