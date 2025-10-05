dadcd89d64e49131d4a6ef07b4cb14abe89e19ea
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-05 19:09:11 +0100

    Added Auth Result class

---
+++ Advertising.Application/Auth/AuthResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Auth
+{
+    public class AuthResult
+    {
+        public bool Success { get; set; }
+        public string? Token { get; set; }
+        public string? Message { get; set; }
+    }
+}
