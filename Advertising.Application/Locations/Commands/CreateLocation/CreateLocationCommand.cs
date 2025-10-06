37257d4bca73baa2a825d45a7afd140584985c83
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 13:38:45 +0100

    Location Command Nd Handler

---
+++ Advertising.Application/Locations/Commands/CreateLocation/CreateLocationCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Locations.Commands.CreateLocation
+{
+    public record CreateLocationCommand(string Name, string State, string Country) : IRequest<int>;
+    
+}
---
+++ Advertising.Application/Locations/Commands/CreateLocation/CreateLocationCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Advertising.Domain.Entities;
+using Advertising.Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Locations.Commands.CreateLocation
+{
+    public class CreateLocationCommandHandler: IRequestHandler<CreateLocationCommand, int>
+    {
+        private readonly AdvertisingDbContext _context;
+        public CreateLocationCommandHandler(AdvertisingDbContext context) => _context = context;
+
+        public async Task<int> Handle(CreateLocationCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Location
+            {
+                Name = request.Name,
+                State = request.State,
+                Country = request.Country
+            };
+
+            _context.Locations.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
