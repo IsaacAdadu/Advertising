ba24a916736e787e32c060cf23c4d8ed74e61e56
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 16:38:54 +0100

    Added Update status command and handler

---
+++ Advertising.Application/Statuses/Commands/UpdateStatus/UpdateStatusCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Statuses.Commands.UpdateStatus
+{
+    public record UpdateStatusCommand(int Id, string Name) : IRequest<bool>;
+    
+}
---
+++ Advertising.Application/Statuses/Commands/UpdateStatus/UpdateStatusHandler.cs
@@ -0,0 +1,27 @@
+﻿using Advertising.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Statuses.Commands.UpdateStatus
+{
+    public class UpdateStatusHandler: IRequestHandler<UpdateStatusCommand, bool>
+    {
+        private readonly AdvertisingDbContext _db;
+        public UpdateStatusHandler(AdvertisingDbContext db) => _db = db;
+
+        public async Task<bool> Handle(UpdateStatusCommand request, CancellationToken cancellationToken)
+        {
+            var status = await _db.Statuses.FirstOrDefaultAsync(s => s.Id == request.Id, cancellationToken);
+            if (status == null) return false;
+
+            status.Name = request.Name.Trim();
+            await _db.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
