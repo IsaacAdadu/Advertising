7aec17a9f8b995e52b2be17ebf436c4b43c9d6c1
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 12:35:52 +0100

    Migration

--- Advertising.Infrastructure/Migrations/20251006112818_Addlocationmanagement.Designer.cs
+++ Advertising.Infrastructure/Migrations/20251006112818_Addlocationmanagement.Designer.cs
@@ -4,6 +4,7 @@ using Advertising.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Advertising.Infrastructure.Migrations
 {
     [DbContext(typeof(AdvertisingDbContext))]
-    partial class AdvertisingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251006112818_Addlocationmanagement")]
+    partial class Addlocationmanagement
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Advertising.Infrastructure/Migrations/20251006112818_Addlocationmanagement.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Advertising.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addlocationmanagement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CampaignLocations_CampaignId",
+                table: "CampaignLocations");
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    State = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CampaignLocations_CampaignId_LocationId",
+                table: "CampaignLocations",
+                columns: new[] { "CampaignId", "LocationId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CampaignLocations_LocationId",
+                table: "CampaignLocations",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CampaignLocations_Location_LocationId",
+                table: "CampaignLocations",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CampaignLocations_Location_LocationId",
+                table: "CampaignLocations");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CampaignLocations_CampaignId_LocationId",
+                table: "CampaignLocations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CampaignLocations_LocationId",
+                table: "CampaignLocations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CampaignLocations_CampaignId",
+                table: "CampaignLocations",
+                column: "CampaignId");
+        }
+    }
+}
