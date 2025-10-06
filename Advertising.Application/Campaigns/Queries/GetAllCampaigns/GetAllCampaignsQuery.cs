ca126156068e38b868078c41d000ba4c3420fb5b
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 15:26:14 +0100

    Added Get all campaign query and handler

---
+++ Advertising.Application/Campaigns/Queries/GetAllCampaigns/GetAllCampaignsQuery.cs
@@ -0,0 +1,13 @@
+﻿using Advertising.Application.Campaigns.Queries.GetCampaignById;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Campaigns.Queries.GetAllCampaigns
+{
+    public record GetAllCampaignsQuery: IRequest<List<CampaignDto>>;
+
+}
---
+++ Advertising.Application/Campaigns/Queries/GetAllCampaigns/GetAllCampaignsQueryHandler.cs
@@ -0,0 +1,54 @@
+﻿using Advertising.Application.Campaigns.Queries.GetCampaignById;
+using Advertising.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Campaigns.Queries.GetAllCampaigns
+{
+    public class GetAllCampaignsQueryHandler: IRequestHandler<GetAllCampaignsQuery, List<CampaignDto>>
+    {
+        private readonly AdvertisingDbContext _context;
+
+        public GetAllCampaignsQueryHandler(AdvertisingDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CampaignDto>> Handle(GetAllCampaignsQuery request, CancellationToken cancellationToken)
+        {
+            var campaigns = await _context.Campaigns
+                .Include(c => c.Banners)
+                .Include(c => c.Locations)
+                .ToListAsync(cancellationToken);
+
+            return campaigns.Select(c => new CampaignDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                From = c.From,
+                To = c.To,
+                Status = c.Status.ToString(),
+                Amount = c.Amount,
+                OwnerId = c.OwnerId,
+                Banners = c.Banners.Select(b => new BannerDto
+                {
+                    Id = b.Id,
+                    Url = b.Url,
+                    PublicId = b.PublicId
+                }).ToList(),
+                Locations = c.Locations.Select(l => new LocationDto
+                {
+                    Id = l.LocationId,
+                    Name = _context.Locations.FirstOrDefault(x => x.Id == l.LocationId)!.Name,
+                    State = _context.Locations.FirstOrDefault(x => x.Id == l.LocationId)!.State,
+                    Country = _context.Locations.FirstOrDefault(x => x.Id == l.LocationId)!.Country
+                }).ToList()
+            }).ToList();
+        }
+    }
+}
