d43751be3a74ad58bd3f4db42005e15990b0a919
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 12:35:08 +0100

    Added update profile method

---
+++ Advertising.API/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Advertising.Application.Auth.Commands.UpdateUserProfile;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advertising.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProfileController:ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProfileController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateProfile([FromBody] UpdateUserProfileCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (!result)
+                return BadRequest("Failed to update profile.");
+            return Ok("Profile updated successfully.");
+        }
+    }
+}
