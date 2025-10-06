42ae95ddc4167f39c6219888dc4a450801d347f3
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 16:39:14 +0100

    commit

---
+++ Advertising.Application/Statuses/Queries/GetStatusById/GetStatusByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using Advertising.Domain.DTOs;
+using Advertising.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Statuses.Queries.GetStatusById
+{
+    public class GetStatusByIdHandler: IRequestHandler<GetStatusByIdQuery, StatusDto?>
+    {
+        private readonly AdvertisingDbContext _db;
+        public GetStatusByIdHandler(AdvertisingDbContext db) => _db = db;
+
+        public async Task<StatusDto?> Handle(GetStatusByIdQuery request, CancellationToken cancellationToken)
+        {
+            var s = await _db.Statuses.AsNoTracking().FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            if (s == null) return null;
+            return new StatusDto { Id = s.Id, Name = s.Name };
+        }
+    }
+}
