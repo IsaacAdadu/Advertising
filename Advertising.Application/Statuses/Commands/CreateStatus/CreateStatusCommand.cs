acd8a5a483fba98689ed1743b8d9cd1659012ed3
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 16:38:21 +0100

    added create status command

---
+++ Advertising.Application/Statuses/Commands/CreateStatus/CreateStatusCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Statuses.Commands.CreateStatus
+{
+    public record CreateStatusCommand(string Name) : IRequest<int>;
+    
+}
---
+++ Advertising.Application/Statuses/Commands/CreateStatus/CreateStatusHandler.cs
@@ -0,0 +1,26 @@
+﻿using Advertising.Domain.Entities;
+using Advertising.Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Statuses.Commands.CreateStatus
+{
+    public class CreateStatusHandler: IRequestHandler<CreateStatusCommand, int>
+    {
+        private readonly AdvertisingDbContext _db;
+
+        public CreateStatusHandler(AdvertisingDbContext db) => _db = db;
+
+        public async Task<int> Handle(CreateStatusCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Status { Name = request.Name.Trim() };
+            _db.Statuses.Add(entity);
+            await _db.SaveChangesAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
