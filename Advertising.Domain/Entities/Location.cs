b5c089c433b820db0c6c31a42c5b37c37c941d0c
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 12:35:42 +0100

    Added a location entity

---
+++ Advertising.Domain/Entities/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Domain.Entities
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string State { get; set; } = default!;
+        public string Country { get; set; } = default!;
+
+        // Optional back reference if you need navigation
+        public List<CampaignLocation> Campaigns { get; set; } = new();
+    }
+}
