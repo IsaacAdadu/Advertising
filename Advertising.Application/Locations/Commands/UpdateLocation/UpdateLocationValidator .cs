29312c21e5d6dff1677cd8df2882a09b5487eb2d
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 15:26:44 +0100

    commit

---
+++ Advertising.Application/Locations/Commands/UpdateLocation/UpdateLocationValidator .cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Locations.Commands.UpdateLocation
+{
+    public class UpdateLocationValidator: AbstractValidator<UpdateLocationCommand>
+    {
+        public UpdateLocationValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0);
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(150);
+            RuleFor(x => x.State).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.Country).NotEmpty().MaximumLength(100);
+        }
+    }
+}
