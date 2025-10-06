ae42670bd1294716cbeb22411d35b2073d64cc31
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 13:39:11 +0100

    Update Location Command and Handler

---
+++ Advertising.Application/Locations/Commands/UpdateLocation/UpdateLocationCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Locations.Commands.UpdateLocation
+{
+    public record UpdateLocationCommand(int Id, string Name, string State, string Country) : IRequest<bool>;
+    
+}
---
+++ Advertising.Application/Locations/Commands/UpdateLocation/UpdateLocationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Advertising.Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Locations.Commands.UpdateLocation
+{
+    public class UpdateLocationCommandHandler: IRequestHandler<UpdateLocationCommand, bool>
+    {
+        private readonly AdvertisingDbContext _context;
+        public UpdateLocationCommandHandler(AdvertisingDbContext context) => _context = context;
+
+        public async Task<bool> Handle(UpdateLocationCommand request, CancellationToken cancellationToken)
+        {
+            var location = await _context.Locations.FindAsync(new object[] { request.Id }, cancellationToken);
+            if (location == null)
+                return false;
+
+            location.Name = request.Name;
+            location.State = request.State;
+            location.Country = request.Country;
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
