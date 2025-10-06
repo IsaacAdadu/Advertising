0e14614f13f8e4bbe9413050fc96b7cd05a50f07
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 15:25:41 +0100

    commit

---
+++ Advertising.Application/Campaigns/Commands/UpdateCampaignStatus/UpdateCampaignStatusCommand.cs
@@ -0,0 +1,13 @@
+﻿using Advertising.Domain.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Campaigns.Commands.UpdateCampaignStatus
+{
+    public record UpdateCampaignStatusCommand(Guid CampaignId, CampaignStatus NewStatus) : IRequest<bool>;
+    
+}
---
+++ Advertising.Application/Campaigns/Commands/UpdateCampaignStatus/UpdateCampaignStatusCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Advertising.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Campaigns.Commands.UpdateCampaignStatus
+{
+    public class UpdateCampaignStatusCommandHandler: IRequestHandler<UpdateCampaignStatusCommand, bool>
+    {
+        private readonly AdvertisingDbContext _context;
+
+        public UpdateCampaignStatusCommandHandler(AdvertisingDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(UpdateCampaignStatusCommand request, CancellationToken cancellationToken)
+        {
+            var campaign = await _context.Campaigns
+                .FirstOrDefaultAsync(c => c.Id == request.CampaignId, cancellationToken);
+
+            if (campaign == null)
+                return false;
+
+            campaign.Status = request.NewStatus;
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
