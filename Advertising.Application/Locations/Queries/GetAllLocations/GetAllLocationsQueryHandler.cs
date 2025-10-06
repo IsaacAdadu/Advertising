05d038e4304ab9bc13aa743051e1d14184fa693d
IsaacAdadu <isaacadaduoche@gmail.com>
2025-10-06 13:39:38 +0100

    Location Query and Handler

---
+++ Advertising.Application/Locations/Queries/GetAllLocations/GetAllLocationsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Advertising.Domain.Entities;
+using Advertising.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertising.Application.Locations.Queries.GetAllLocations
+{
+    public class GetAllLocationsQueryHandler: IRequestHandler<GetAllLocationsQuery, List<Location>>
+    {
+        private readonly AdvertisingDbContext _context;
+        public GetAllLocationsQueryHandler(AdvertisingDbContext context) => _context = context;
+
+        public async Task<List<Location>> Handle(GetAllLocationsQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Locations.AsNoTracking().ToListAsync(cancellationToken);
+        }
+    }
+}
